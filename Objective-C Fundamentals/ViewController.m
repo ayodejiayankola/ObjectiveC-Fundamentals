fc89d239c5a98585ff17e828ae133e8e81f83e7e
ayodejiayankola <ayodejiayankola@gmail.com>
2021-03-23 14:09:02 +0100

    variables

--- Objective-C Fundamentals/ViewController.m
+++ Objective-C Fundamentals/ViewController.m
@@ -16,6 +16,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    unsigned short myShort = 10000; // positive value
+   static short myShort2 = 10000; // the value persist over all function call
+    const short myShort3 = 600; // value can not be modified
+    int myInt = 600;
+    char myChar = "a";
+    float myFloat = 500.0f;
+    double myDouble = 600.0;
+    BOOL myBool = true;
+    id myObject = self; //an instance of the view controller
+    
+    
+    
+    
 }
 
 
